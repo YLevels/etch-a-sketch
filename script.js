b70c647a2175c8444e00968662e93b3a0b8f478e
Yeidy Levels <ylevels@schools.nyc.gov>
2022-08-27 20:42:55 -0400

    HTML and CSS on load done (with slider!)

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const gridContainer = document.querySelector(".grid-container");
+var slider = document.querySelector(".slider");
+var output = document.querySelector(".size");
+
 let cell = document.createElement("div");
 
 let size = 16;
+output.innerHTML = "16x16";
+
+slider.oninput = function () {
+    output.innerHTML = `${this.value + "x" + this.value}`;
+}
 
 function makeDefaultGrid (size) {
     for (i=1; i<=size*size; i++) {
         cell = document.createElement("div");
         cell.style.borderStyle = "solid";
         cell.style.borderColor = "black";
-        cell.style.border
+        cell.style.borderWidth = "thin";
         gridContainer.appendChild(cell);
     }
 }
